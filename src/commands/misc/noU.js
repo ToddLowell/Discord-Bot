0dc35d618c0c196a88247258a5b71cc36fedc5d7
Raaed Kabir <raaed.kabir@gmail.com>
2021-02-08 19:04:27 +0800

    added \noU command

---
+++ src/commands/misc/noU.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const Discord = require('discord.js');
+
+module.exports = {
+  commands: ['noU', 'reverseCard'],
+  description: `Unleash the power of "no u" and win any argument using the ultimate comeback card that nobody can resist.`,
+  callback(msg, args, text) {
+    const embed = new Discord.MessageEmbed()
+      .setColor('#66fcf1')
+      .attachFiles(path.join(__dirname, '../../assets/noU.png'))
+      .setImage('attachment://noU.png');
+
+    msg.channel.send(embed);
+  },
+};
