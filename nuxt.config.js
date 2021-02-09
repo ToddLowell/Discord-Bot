149c7d801f2ad3ad272045018de68b7d81cae0b0
Raaed Kabir <raaed.kabir@gmail.com>
2021-02-09 19:20:22 +0800

    added social meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,80 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'website',
+    title: 'Aigis - Discord Bot',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'A fun and easy to use bot for Discord. Made for myself and friends but available to anyone who wants to give it a spin.',
+      },
+
+      // OpenGraph tags
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Aigis - Discord Bot',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'A fun and easy to use bot for Discord. Made for myself and friends but available to anyone who wants to give it a spin.',
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://aigis-discord-bot.herokuapp.com/',
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://aigis-discord-bot.herokuapp.com/avatar.jpg',
+      },
+
+      // Twitter tags
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary',
+      },
+      {
+        hid: 'twitter:site',
+        name: 'twitter:site',
+        content: '@raaedkabir',
+      },
+      {
+        hid: 'twitter:creator',
+        name: 'twitter:creator',
+        content: '@raaedkabir',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://aigis-discord-bot.herokuapp.com/avatar.jpg',
+      },
+
+      // iOS tags
+      {
+        name: 'apple-mobile-web-app-capable',
+        content: 'yes',
+      },
+      {
+        name: 'apple-mobile-web-app-status-bar-style',
+        content: 'black',
+      },
+      {
+        name: 'apple-mobile-web-app-title',
+        content: 'Aigis - Discord Bot',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
