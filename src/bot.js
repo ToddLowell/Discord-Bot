86335d18c1ec3ae5319658780b6f9e49982de71b
Raaed Kabir <raaed.kabir@gmail.com>
2021-02-09 11:29:26 +0800

    set maxListeners to infinity

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.setMaxListeners(0);
+
 require('./voiceStatusUpdate.js')(client);
 
 client.on('ready', () => {
