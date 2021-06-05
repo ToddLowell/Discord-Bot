051187deb64574a8ffedfe93d7806d0eb4b11d14
Raaed Kabir <raaed.kabir@gmail.com>
2021-06-05 09:30:32 +0800

    added avatar command

---
+++ src/commands/user/avatar.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  commands: 'avatar',
+  expectedArgs: '@someone',
+  description: "Get a User's Avatar",
+  minArgs: 0,
+  maxArgs: 1,
+  callback(msg, args, text) {
+    console.log({ msg, args });
+
+    if (msg.mentions.everyone)
+      return msg.channel.send(`You can't use @everyone or @here.`);
+
+    if (msg.mentions.roles.size)
+      return msg.channel.send(`Tag a person, not a role.`);
+
+    const member = msg.mentions.users.first() || msg.author;
+    const avatar = member.displayAvatarURL({
+      format: 'png',
+      dynamic: true,
+      size: 4096,
+    });
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`${member.username}'s avatar`)
+      .setImage(avatar)
+      .setColor('#66fcf1')
+      .setTimestamp();
+
+    msg.channel.send(embed);
+  },
+};
